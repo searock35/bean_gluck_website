a044df4a7bec7638f4393d9524ac7685270b00e9
searock35 <searock35@gmail.com>
2020-08-22 11:22:15 -0700

    Added framework for book-gathering API that will link to database

---
+++ website_portion_react/src/api/books.js
@@ -0,0 +1,28 @@
+
+//API class that will manage book search engine. Will recieve the current search entry as
+//input and return the list of matches.
+
+class BooksAPI {
+    
+
+    getAutoComplete(searchString) {
+
+        console.log("search string", searchString);
+
+        const booksArray = [
+            { title: "Madagascar Unlimited", author: "Randall", edition: "8th", id: "1" },
+            { title: "Django Unhinged", author: "Corey", edition: "8th", id: "2" },
+            { title: "Django 2", author: "Corey", edition: "8th", id: "3" },
+            { title: "Esperanza Rising", author: "Mr. Horning", edition: "8th", id: "4" },
+            { title: "Hunger Games", author: "JK Rowling", edition: "8th", id: "5" },
+            { title: "Kryptonite Sucks", author: "Superman", edition: "8th", id: "6" },
+        ]
+
+        if(searchString==='') return [];
+        return booksArray;
+    }
+
+}
+
+
+export default new BooksAPI();
