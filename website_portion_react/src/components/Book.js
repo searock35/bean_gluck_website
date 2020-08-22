a9bfc21a9a5e1068cdac1e62976cc27eb7517c0d
searock35 <searock35@gmail.com>
2020-08-22 15:55:14 -0700

    Split home page into several components to better represent its respective elements. Added functionality to click on a book and enter a search form

---
+++ website_portion_react/src/components/Book.js
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import UserContext from '../userComponents/UserContext';
+
+
+
+//the book item will recieve the information from the database as props.bookInfo
+function Book(props) {
+
+    const history = useHistory();
+    const bookInfo = props.bookInfo;
+    const userInfo = useContext(UserContext);
+
+    //redirects user to book search page with bookID as the 
+    const linkHandler = (bookId) => history.push("/book-search/?bookId=" + bookId + "&schoolId=" + userInfo.schoolId);
+
+    let idMessage = "";
+    if(bookInfo.id.length < 10 || bookInfo.idIsCustom) {
+        idMessage = "ID: " + bookInfo.id;
+    } else {
+        idMessage = "ISBN: " + bookInfo.id;
+    }
+
+    let amountMessage = "Listings: ";
+    if(bookInfo.amount) amountMessage += bookInfo.amount;
+    else amountMessage+="None";
+    
+    return (
+    <ul className="book-info" onClick={() => linkHandler(bookInfo.id)}>
+        <li className="book-title">Title: {bookInfo.title}</li>
+        <li className="book-edition">Edition: {bookInfo.edition}</li>
+        <li className="book-author">Book Author: {bookInfo.author}</li>
+        {/* Add "not available" option if book ID is other format for different book type */}
+        <li className="book-id">{idMessage}</li> 
+        <li className="book-amount">{amountMessage}</li>
+    </ul>
+    )
+
+
+}
+
+export default Book;
---
+++ website_portion_react/src/components/BookSearchForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+//API's
+import BooksAPI from '../api/books';
+//Components
+import Book from './Book';
+
+function BookDropDown(props) {
+
+    const allBooks = props.books.map( (book) => 
+    <li className="book-drop-down-entry" key={book.id}>
+        <Book bookInfo={book} />
+    </li>
+    )
+
+    return(<ul>{allBooks}</ul>);
+}
+
+function BookSearchForm(props) {
+
+    const defaultSearchString = 'Enter book ISBN, Title, Author';
+    const [searchText, setSearchText] = useState(defaultSearchString); 
+    const [bookList, setBookList] = useState([]);  
+
+    const onSearchSubmit = (e) => {
+        console.log("form submitted");
+        e.preventDefault();
+    }
+        
+    function onChangeSearchText(e) {
+        setSearchText(e.target.value);
+
+        //sets state of book list from result by Books API
+        setBookList(BooksAPI.getAutoComplete(e.target.value));
+    }
+
+    return (
+        <form onSubmit={onSearchSubmit}>
+            <div id="book_search_bar" className="form-group"><label>Book Title/ISBN:</label>
+                <input 
+                    id="mainSearch"
+                    className="form-control form-control-lg"
+                    value={searchText}
+                    name="name"
+                    type="text"
+                    onClick={(e) => (e.target.value===defaultSearchString) && setSearchText("")}
+                    onChange={onChangeSearchText}
+                />
+            </div>
+            <div className="form-group bookDropDown">
+                <BookDropDown books={bookList} />
+            </div>
+            <div className="form-group">
+                Select your school:
+                <select name="school_select">
+                        <option value="messiah">Messiah University</option>
+                        <option value="penn_state">Penn State University</option>
+                    </select>
+                <input defaultChecked="checked" name="local" type="checkbox" value="daily" /> Include Local Searches <br />
+                <input id="form_submission" type="submit" value="Search" />
+            </div>
+        </form>
+    )
+
+
+
+}
+
+export default BookSearchForm;
