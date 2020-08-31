a5faec7a4ddb154f96d0d94938f14618d788e269
searock35 <searock35@gmail.com>
2020-08-30 19:44:37 -0700

    Added registration page for user creation.

--- website_portion_react/src/components/auth/Register.js
+++ website_portion_react/src/components/auth/Register.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
+import { Form, Col, Button } from "react-bootstrap";
 
 function Register() {
 
     const [user, setUser] = useState({
         firstName: "",
         lastName: "",
-        username: "",
         password: "",
-        passwordValidate: "",
         email: "",
         school: ""
     })
+
+    const [errors, setErrors] = useState({
+        ...user
+    });
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -18,34 +21,101 @@ function Register() {
     }
 
     const onChangeHandler = (e) => {
-        console.log(user.lastName);
-        const { name, value } = e.target;
+        const { id, value } = e.target;
         setUser((user) => ({
             ...user,
-            [name]: value
+            [id]: value
         }))
+        
+        validate({
+            [id]: value
+        });
+
     }
 
-    const TextInput = ({ name, value, ...rest }) => (<input 
-            type="text"
-            name={name}
-            key={name}
-            value={value}
-            {...rest}
-        />)
+    const setError = (name, value) => {
+        setErrors({
+            ...errors,
+            [name]: value,
+        })
+    }
 
+    const validate = values => {
+      
+        if (!values.firstName) {
+            setError("firstName", "Required");
+        } else if (values.firstName === "admin") {
+            setError("firstName", "Nice try!");
+        }
+        
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            setError("email", "Invalid email");
+        } else {
+            setError("email", "");
+        }
+    
 
-
-
+        if (values.password.length >= 8 && values.password.length < 32) {
+            setError("password", "");
+        } else if (values.password.length===0) {
+            setError("password", "Required");
+        } else {
+            setError("password", "Invalid Password");
+        }
+      };
 
     return (
-        <div key="divKey">
-            <form onSubmit={submitHandler} key="formkey">
-                First Name:<input type="text" name="firstName" value={user.firstName} onChange={onChangeHandler}/>
-                Last Name:<TextInput name="lastName" value={user.lastName} onChange={onChangeHandler}/>
-                Password: <TextInput name="password" value={user.password} onChange={onChangeHandler}/>
-            </form>
-        </div>
+        <Form onSubmit={submitHandler}>
+            <h1>User Registration</h1>
+            <Form.Group controlId="fullName">
+                <Form.Label>Name</Form.Label>
+                <Form.Row>
+                    <Col>
+                        <Form.Group controlId="firstName">
+                            <Form.Control type="name" placeholder="First name" value={user.firstName} onChange={onChangeHandler} />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="lastName">
+                            <Form.Control type="name" placeholder="Last name" value={user.lastName} onChange={onChangeHandler} />
+                        </Form.Group>
+                    </Col>
+                </Form.Row>
+                <Form.Text className="text-muted">
+                    This is the name your peers will see for your book listings.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" value={user.email} onChange={onChangeHandler} />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else. <span style={{color: 'red'}}>{errors.email}</span>
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={user.password} onChange={onChangeHandler} />
+                <Form.Text className="text-muted">
+                    Password must be longer than 8 characters, and must include 1 number and 1 special character. <span style={{color: "red"}}>{errors.password}</span>
+                </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="school">
+                <Form.Label>School</Form.Label>
+                <Form.Control type="school" placeholder="Search for your school" value={user.school} onChange={onChangeHandler} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+        // <div key="divKey">
+        //     <form onSubmit={submitHandler} key="formkey">
+        //         First Name:<input type="text" name="firstName" value={user.firstName} onChange={onChangeHandler}/>
+        //         Last Name:<TextInput name="lastName" value={user.lastName} onChange={onChangeHandler}/>
+        //         Password: <TextInput name="password" value={user.password} onChange={onChangeHandler}/>
+        //     </form>
+        // </div>
     );
 }
 
