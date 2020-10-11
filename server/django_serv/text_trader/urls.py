deb5196465bba1cd6c44058f3e379ca7e0171467
Corey Bean <searock35@gmail.com>
2020-10-11 00:55:13 -0400

    Added api tag to denote api portion of server

--- server/django_serv/text_trader/urls.py
+++ server/django_serv/text_trader/urls.py
@@ -3,15 +3,15 @@ from text_trader import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('books/', views.BookList.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetail.as_view(), name='book-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('listings/', views.ListingList.as_view(), name='listing-list'),
-    path('listings/<int:pk>/', views.ListingDetail.as_view(), name='listing-detail'),
-    path('requests/', views.ListingRequestList.as_view(), name='request-list'),
-    path('requests/<int:pk>/', views.ListingRequestDetail.as_view(), name='request-detail'),
-    path('', views.api_root),
+    path('api/books/', views.BookList.as_view(), name='book-list'),
+    path('api/books/<int:pk>/', views.BookDetail.as_view(), name='book-detail'),
+    path('api/users/', views.UserList.as_view(), name='user-list'),
+    path('api/users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('api/listings/', views.ListingList.as_view(), name='listing-list'),
+    path('api/listings/<int:pk>/', views.ListingDetail.as_view(), name='listing-detail'),
+    path('api/requests/', views.ListingRequestList.as_view(), name='request-list'),
+    path('api/requests/<int:pk>/', views.ListingRequestDetail.as_view(), name='request-detail'),
+    path('api/', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
