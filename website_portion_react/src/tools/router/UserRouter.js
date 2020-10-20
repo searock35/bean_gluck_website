11b307e91f5b13709b50c94ddae6a9718e8fdab3
searock35 <searock35@gmail.com>
2020-10-20 12:17:25 -0400

    Added dabase models, minor file changes

--- website_portion_react/src/tools/router/UserRouter.js
+++ website_portion_react/src/tools/router/UserRouter.js
@@ -9,19 +9,15 @@ import Dashboard from '../../components/user/dashboard/Dashboard';
 import EditUserProfile from'../../components/user/EditUserProfile';
 import Unauthorized from '../../components/user/Unauthorized';
 
-
 //This route will be used for all locations starting with ./user/:username, and will redirect accordingly.
 //Should deny access to user routes if the :username parameter doesn't match the authorized username.
-function UserRouter() {
+ function UserRouter() {
 
     const currentUser = useContext(UserContext);
-
     let { url } = useRouteMatch();
-    
     const { username } = useParams();
-
-    if(username !== currentUser.username) return (<Redirect to="/user/unauthorized"/>)
     
+    if(username !== currentUser.username) return (<Redirect to="/user/unauthorized"/>)
     return (
         
         <Switch>
